db4046d118442744dd5915b96114e125339f9af0
rykugur <rollhax@gmail.com>
2025-03-10 20:02:50 -0500

    lol

--- src/Eve-O-Preview/Services/Implementation/WindowManager.cs
+++ src/Eve-O-Preview/Services/Implementation/WindowManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using EveOPreview.Configuration;
 using EveOPreview.Services.Interop;
+using System.Collections.Generic;
 
 namespace EveOPreview.Services.Implementation
 {
